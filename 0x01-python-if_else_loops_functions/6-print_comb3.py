4fb9c36ed4f9d61d3cc36e7de5d027089b8d175f
ConquerorCletus <smartchibuike2019@gmail.com>
2023-05-09 13:54:33 +0000

    6-print combination

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 for i in range(0, 10):
     for j in range(1, 10):
-        print("{}{},".format(i, j), end=" ")
+        print("{}{}".format(i, j), end=" ")
+        if i != 8 or j != 9:
+            print(", ", end="")
+print()
