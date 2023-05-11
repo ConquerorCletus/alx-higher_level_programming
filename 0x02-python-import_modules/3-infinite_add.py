08ad25922efd0082fc422b94799dd4d7b57c0f68
ConquerorCletus <smartchibuike2019@gmail.com>
2023-05-11 14:42:43 +0000

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,4 +4,4 @@ if __name__ == "__main__":
     total_index = 0
     for i in range(len(argv) - 1):
         total_index += (int(argv[i + 1]))
-        print("{:d}".format(total_index))
+    print("{:d}".format(total_index))
