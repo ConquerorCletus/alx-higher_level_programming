977153adb78497b349479ea848a15cba5a805e67
ConquerorCletus <smartchibuike2019@gmail.com>
2023-05-11 13:58:41 +0000

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    index = len(argv) - 1
+
+    if index == 0:
+        print("0 arguments.")
+    elif index == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(index))
+    for i in range(index):
+        print("{}: {}".format(i + 1, argv[i + 1]))
