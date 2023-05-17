6f052c849eb1b1af267a42fc5f257afcb889ff48
ConquerorCletus <smartchibuike2019@gmail.com>
2023-05-17 19:31:08 +0000

    roman figures

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,9 +2,17 @@
 def roman_to_int(roman_string):
     if not roman_string or type(roman_string) != str:
         return 0
-    numbers = 0
-    roman_numerals = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    num = 0
+    roman_numerals = {
+            'M': 1000,
+            'D': 500,
+            'C': 100,
+            'L': 50,
+            'X': 10,
+            'V': 5,
+            'I': 1
+            }
     for i in reversed(roman_string):
         roman_figures = roman_numerals[i]
-        numbers += roman_figures if numbers < roman_figures * 5 else -roman_figures
-    return numbers
+        num += roman_figures if num < roman_figures * 5 else -roman_figures
+    return num
