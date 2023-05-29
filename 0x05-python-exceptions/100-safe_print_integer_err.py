9866a89615f144adbdf269258150ee59f72f8da4
ConquerorCletus <smartchibuike2019@gmail.com>
2023-05-29 18:34:23 +0000

    stderr and exception handling

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,7 +4,7 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
     except Exception as i:
-        sys.stderr.write("Exception: {}\n".format(i))
+        stderr.write("Exception: {}\n".format(i))
         return (False)
     else:
         return (True)
